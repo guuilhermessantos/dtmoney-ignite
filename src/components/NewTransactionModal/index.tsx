c3c3e166a456019f64d3e333c6e4258a73b976fe
Guilherme Santos <gsantos@action.local>
2021-08-24 08:33:20 -0300

    Mirajejs e Estilização basica do modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,50 @@
+import Modal from 'react-modal';
+import { Container } from './styles';
+import closeImg from '../../assets/close.svg'
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+
+
+Modal.setAppElement('#root');
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+
+  return (
+    <Modal isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+       >
+         <button
+          type="button"
+          onClick={onRequestClose}
+          className="react-modal-close"
+        >
+           <img src={closeImg} alt="Fechar modal" />
+         </button>
+       <Container>
+      <h2>Cadastrar transação</h2>
+
+      <input
+        placeholder="Titulo"
+      />
+
+      <input
+       type="number"
+       placeholder="Valor" 
+      />
+
+      <input type="" placeholder="Categoria" />
+
+      <button type="submit">Cadastrar</button>
+
+      
+
+       </Container>
+    </Modal>
+  )
+}
